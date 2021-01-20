530daa6cde8f53e0fd2100b99c380862c5ae30a7
thanghaqn2000 <57361234+thanghaqn2000@users.noreply.github.com>
2021-01-20 18:12:07 +0700

    static

---
+++ module2/src/access_modifier_static_method_static_property/bai_tap/access_modifer/MainCircle.java
@@ -0,0 +1,10 @@
+package access_modifier_static_method_static_property.bai_tap.access_modifer;
+
+public class MainCircle {
+    public static void main(String[] args) {
+        Circle c1=new Circle();
+        System.out.println(c1.getColor());
+        System.out.println(c1.getRadius());
+
+    }
+}
---
+++ module2/src/access_modifier_static_method_static_property/thuc_hanh/access_modier/AccessModier.java
@@ -0,0 +1,21 @@
+package access_modifier_static_method_static_property.thuc_hanh.access_modier;
+
+class A{
+
+    private int data=40;
+
+    private void msg(){System.out.println("Hello java");}
+
+}
+public class AccessModier {
+    public static void main(String args[]){
+
+        A obj=new A();
+
+        //System.out.println(obj.data);//Compile Time Error
+
+        //obj.msg();//Compile Time Error
+
+    }
+
+}
